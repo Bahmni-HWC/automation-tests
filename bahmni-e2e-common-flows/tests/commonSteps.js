3597e28918c88a71069eb3c0452e1d689820773c
gokultw <gokul.kuppan@thoughtworks.com>
2023-10-13 17:45:10 +0530

    Gh-pages

--- bahmni-e2e-common-flows/tests/commonSteps.js
+++
@@ -1,9 +0,0 @@
-const { click, $, goBack } = require("taiko");
-
-step("Click <buttonName> Tab on Inventory", async function (strButtonName) {
-	await click($("//BUTTON[normalize-space()='" + strButtonName + "' and @role='tab']"));
-});
-
-step("Click browser back button", async function() {
-	await goBack();
-});
--- tests/Dummy.js
+++
@@ -1,4 +0,0 @@
-step("DummyStep_To_Avoid_Error", async function() {
-	throw 'Unimplemented Step';
-	//this is needed to avoid IDE crashing.
-});
