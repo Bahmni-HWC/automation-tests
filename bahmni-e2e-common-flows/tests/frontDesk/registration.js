dfc4b86da7681616b57cc9874aaf3e4e2c1e3a71
gokultw <gokul.kuppan@thoughtworks.com>
2023-10-13 17:57:56 +0530

    Close visit fix

--- bahmni-e2e-common-flows/tests/frontDesk/registration.js
+++ bahmni-e2e-common-flows/tests/frontDesk/registration.js
@@ -459,6 +459,7 @@ step("Get patient ID from patient dashboard", async function () {
 
 step("Click visit attributes link", async function () {
     await click(link("Visit Attributes"), { waitForNavigation: true, navigationTimeout: process.env.actionTimeout });
+    await waitFor(1000)
     await switchTo(/registration/);
     await closeTab(/clinical/);
 });
